21e95323d36211646c08e672a962048a2411d868
Jederson Andre <andre_sjx@live.com>
2024-03-08 22:14:25 -0300

    :sparkles: feat: implementado logica pra exibir screen de login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import Login from './app/screens/login/login';
+import { color } from './app/utils/colors';
+import { GochiHand_400Regular, useFonts } from '@expo-google-fonts/gochi-hand'
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  let [fontsLoaded, fontError] = useFonts({
+    GochiHand_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return <Text>Fonte não carregada</Text>;
+  }
+
+  SplashScreen.hideAsync()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Login />
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: color.background.primary,
+    flex: 1
   },
-});
+
+})
