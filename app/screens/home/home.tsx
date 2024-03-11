142409de86c3990bef5e81590cf7af26da66b31e
Jederson Andre <andre_sjx@live.com>
2024-03-11 19:48:43 -0300

    :recycle: refactor: add fonte poppins, e inclusao de propriedade para compoenente filho

--- app/screens/home/home.tsx
+++ app/screens/home/home.tsx
@@ -3,30 +3,41 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import MessageSend from "../../components/tabNavigation/messageSend";
 import MessageScheduled from "../../components/tabNavigation/messageScheduled";
 import { color } from "../../utils/colors";
+import NewSchedule from '../../components/button/newSchedule';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Schedule from '../schedule/schedule';
+import Header from '../../components/header/header';
 
 const Tab = createMaterialTopTabNavigator();
+const Stack = createNativeStackNavigator();
 
-const Home = () => {
+const Home = ({ navigation }: any) => {
     return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarStyle: {
-                    backgroundColor: color.background.secundary,
-                },
-                tabBarLabelStyle: {
-                    fontSize: 16
-                },
-                tabBarIndicatorStyle: {
-                    borderBottomWidth: 2,
-                    borderBottomColor: color.icons
-                },
-                tabBarActiveTintColor: color.text.secundary,
-                tabBarInactiveTintColor: color.text.primary,
+        <>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarStyle: {
+                        backgroundColor: color.background.secundary,
+                    },
+                    tabBarLabelStyle: {
+                        fontSize: 16,
+                        fontFamily: 'GochiHand_400Regular'
+                    },
+                    tabBarIndicatorStyle: {
+                        borderBottomWidth: 2,
+                        borderBottomColor: color.icons.form
+                    },
+                    tabBarActiveTintColor: color.text.secundary,
+                    tabBarInactiveTintColor: color.text.primary,
 
-            }}>
-            <Tab.Screen name="Mens. Enviadas" component={MessageSend} />
-            <Tab.Screen name="Mens. Agendadas" component={MessageScheduled} />
-        </Tab.Navigator>
+                }}>
+                <Tab.Screen name="Mens. Enviadas" component={MessageSend} />
+                <Tab.Screen name="Mens. Agendadas" component={MessageScheduled} />
+            </Tab.Navigator>
+
+            <NewSchedule navigation={navigation} />
+
+        </>
     )
 }
 
