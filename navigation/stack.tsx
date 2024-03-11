0a649eff712f7d85f934f5b77e056f7bf52f69ab
Jederson Andre <andre_sjx@live.com>
2024-03-11 19:50:07 -0300

    :recycle: refactor: add nova tela de navegação

--- navigation/stack.tsx
+++ navigation/stack.tsx
@@ -2,9 +2,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../app/screens/login/login';
 import Header from '../app/components/header/header'
 import { color } from '../app/utils/colors';
-import { GochiHand_400Regular } from '@expo-google-fonts/gochi-hand';
-import { Image, Text } from 'react-native';
 import Home from '../app/screens/home/home';
+import Schedule from '../app/screens/schedule/schedule';
 const Stack = createNativeStackNavigator();
 
 const StackNavigation = () => {
@@ -34,6 +33,24 @@ const StackNavigation = () => {
 
                 }}
             />
+            <Stack.Screen
+                name="Schedule"
+                component={Schedule}
+                options={{
+                    headerTitle: () => <Header />,
+                    headerBackVisible: false,
+                    headerStyle: {
+                        backgroundColor: color.background.secundary,
+                    },
+                    headerTintColor: color.text.primary,
+                    headerTitleStyle: {
+                        fontFamily: 'GochiHand_400Regular',
+                        fontSize: 22,
+                    },
+
+                }}
+            />
+
         </Stack.Navigator>
     )
 }
